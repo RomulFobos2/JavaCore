e84732b7e85dbee46ba7e4e328004e45684bac8c
romulfobos <romulfobos@gmail.com>
2022-08-30 03:39:02 +0500

    Done

--- HomeWork1/task2/src/main/java/Main.java
+++ HomeWork1/task2/src/main/java/Main.java
@@ -1,2 +1,65 @@
+import java.util.Arrays;
+
 public class Main {
+
+    /*
+    2. Отсортируйте массив [5,6,3,2,5,1,4,9]
+     */
+    public static void main(String[] args) {
+        int[] workArray = {5, 6, 3, 2, 5, 1, 4, 9};
+        System.out.println("Исходный массив = " + Arrays.toString(workArray));
+        quickSort(workArray, 0, workArray.length - 1, true);
+        System.out.println("Массив отсортирован по возрастанию = " + Arrays.toString(workArray));
+        quickSort(workArray, 0, workArray.length - 1, false);
+        System.out.println("Массив отсортирован по убыванию = " + Arrays.toString(workArray));
+    }
+
+    /*
+        Сортируем массив методом "Быстрой сортировки"
+        order - true - по возрастнаию, false - по убыванию
+     */
+    public static void quickSort(int[] array, int low, int height, boolean order) {
+        if (array.length == 0 | low >= height) {
+            return;
+        }
+        int middleElement = array[(low + height) / 2];
+        int i = low;
+        int j = height;
+
+        while (i <= j) {
+            if (order){
+                while (array[i] < middleElement) {
+                    i++;
+                }
+                while (array[j] > middleElement) {
+                    j--;
+                }
+            }
+            else {
+                while (array[i] > middleElement) {
+                    i++;
+                }
+                while (array[j] < middleElement) {
+                    j--;
+                }
+            }
+
+            if (i <= j) {
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+                i++;
+                j--;
+            }
+
+        }
+
+        if (low < j) {
+            quickSort(array, low, j, order);
+        }
+
+        if (height > i) {
+            quickSort(array, i, height, order);
+        }
+    }
 }
